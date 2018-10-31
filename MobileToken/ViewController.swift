546f6bbee503a35698caf201455da8060d4616cb
hamed akhlaghi <akhlaghi65@gmail.com>
2018-10-31 15:18:51 +0330

    Setup closed #1

---
+++ MobileToken/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MobileToken
+//
+//  Created by hamed akhlaghi on 8/9/1397 AP.
+//  Copyright © 1397 ba24.ir. All rights reserved.
+//
+
+import UIKit
+import ObjectMapper
+import RealmSwift
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,18 @@
+platform :ios, '9.0'
+
+target 'MobileToken' do
+    
+    use_frameworks!
+    pod "ObjectMapper+Realm", '~> 0.6'
+    
+    target 'MobileTokenTests' do
+        inherit! :search_paths
+        # Pods for testing
+    end
+    
+    target 'MobileTokenUITests' do
+        inherit! :search_paths
+        # Pods for testing
+    end
+    
+end
