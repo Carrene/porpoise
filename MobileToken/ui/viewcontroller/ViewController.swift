2b98a1b93c163404bea6692983af0acaeab067d1
hamed akhlaghi <akhlaghi65@gmail.com>
2018-11-03 19:31:12 +0330

    Progress with progress view #6

--- MobileToken/ui/viewcontroller/ViewController.swift
+++ MobileToken/ui/viewcontroller/ViewController.swift
@@ -11,14 +11,27 @@ import ObjectMapper
 import RealmSwift
 class ViewController: UIViewController {
 
+    @IBOutlet weak var progressView: UIProgressView!
     @IBOutlet var width: NSLayoutConstraint!
     @IBOutlet weak var lbOtp: UILabel!
     @IBOutlet weak var animeView: UIView!
+    var timer: Timer?
     var frame: CGRect?
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        
+        progressView.progress = 0
+        progressView.trackTintColor = .white
+        progressView.progressTintColor = .red
+        self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        progressView.progress += 0.01
+        if progressView.progress >= 1 {
+            progressView.progress = 0
+            // generate new otp
+        }
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -44,5 +57,11 @@ class ViewController: UIViewController {
             
         })
     }
+    @IBAction func resetProgress(_ sender: Any) {
+        self.width.constant = 0
+        self.progressView.progress = 0
+        self.start(self)
+    }
+
 }
 
