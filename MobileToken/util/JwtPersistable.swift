1d61b181e1888d276d45fb9249b046fd057e3c30
hamed akhlaghi <akhlaghi65@gmail.com>
2018-11-14 13:32:47 +0330

    UserDefaultsJwtPersistor closed #26

--- MobileToken/util/JwtPersistable.swift
+++ MobileToken/util/JwtPersistable.swift
@@ -11,6 +11,14 @@ import Foundation
 public protocol JwtPersistable {
     
     func get() -> String?
-    func save(jwt: String)
-    func delete()
+    func get(identifier: String) -> String?
+    func getAll() -> [String]?
+    func save(jwt: String) -> Bool?
+    func save(jwt: String, identifier: String) -> Bool?
+    func delete() -> Bool?
+    func delete(identifier: String) -> Bool?
+    func deleteAll() -> Bool?
+    func size() -> Int?
+    func getIdentifier(jwt: String) -> String?
+    func contains(identifier: String) -> Bool?
 }
--- MobileToken/util/UserDefaultsJwtPersistor.swift
+++ MobileToken/util/UserDefaultsJwtPersistor.swift
@@ -11,7 +11,7 @@ import Foundation
 class UserDefaultsJwtPersistor: JwtPersistable {
     
     fileprivate var userDefaults: UserDefaults
-    public let JWT_KEY: String = "JWT_KEY"
+    public let JWT_KEY: String = "jwt"
     
     init(userDefaults: UserDefaults = UserDefaults.standard) {
         self.userDefaults = userDefaults
@@ -21,11 +21,73 @@ class UserDefaultsJwtPersistor: JwtPersistable {
         return userDefaults.string(forKey: JWT_KEY)
     }
     
-    func save(jwt: String) {
-        userDefaults.set(jwt, forKey: JWT_KEY)
+    func get(identifier: String) -> String? {
+        return userDefaults.string(forKey: JWT_KEY + identifier)
+
     }
     
-    func delete() {
+    func getAll() -> [String]? {
+        var jwtList = [String]()
+        for element in userDefaults.dictionaryRepresentation() {
+            if  element.key.hasPrefix(JWT_KEY) {
+                jwtList.append(element.value as! String)
+            }
+        }
+        return jwtList
+    }
+    
+    func save(jwt: String) -> Bool? {
+        userDefaults.set(jwt, forKey: JWT_KEY)
+        return userDefaults.string(forKey: JWT_KEY) == jwt
+    }
+    
+    func save(jwt: String, identifier: String) -> Bool? {
+        userDefaults.set(jwt, forKey: JWT_KEY + identifier)
+        return userDefaults.string(forKey: JWT_KEY + identifier) == jwt
+    }
+    
+    func delete() -> Bool? {
         userDefaults.removeObject(forKey: JWT_KEY)
+        return userDefaults.string(forKey: JWT_KEY) == nil
+    }
+    
+    func delete(identifier: String) -> Bool? {
+        userDefaults.removeObject(forKey: JWT_KEY + identifier)
+        return userDefaults.string(forKey: JWT_KEY + identifier) == nil
+    }
+    
+    func deleteAll() -> Bool? {
+        for element in userDefaults.dictionaryRepresentation() {
+            if element.key.hasPrefix(JWT_KEY) {
+                userDefaults.removeObject(forKey: element.key)
+                if userDefaults.string(forKey: element.key) != nil {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+    
+    func size() -> Int? {
+        var i = 0
+        for element in userDefaults.dictionaryRepresentation() {
+            if  element.key.hasPrefix(JWT_KEY) {
+                i += 1
+            }
+        }
+        return i
+    }
+    
+    func getIdentifier(jwt: String) -> String? {
+        for element in userDefaults.dictionaryRepresentation() {
+            if element.value as! String == jwt {
+                return element.key
+            }
+        }
+        return nil
+    }
+    
+    func contains(identifier: String) -> Bool? {
+        return userDefaults.string(forKey: JWT_KEY + identifier) != nil
     }
 }
