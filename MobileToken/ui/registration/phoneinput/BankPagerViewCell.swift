f8df9031424971cef0c54d8a30ead7dace03cbf8
hamed akhlaghi <akhlaghi65@gmail.com>
2018-11-14 16:13:11 +0330

    Bank pager view adapter and cell closed #29

---
+++ MobileToken/ui/registration/phoneinput/BankPagerViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  BankPagerViewCell.swift
+//  MobileToken
+//
+//  Created by hamed akhlaghi on 8/23/1397 AP.
+//  Copyright © 1397 ba24.ir. All rights reserved.
+//
+
+import Foundation
+import FSPagerView
+class BankPagerViewCell: FSPagerViewCell {
+    
+    @IBOutlet weak var lbBankName: UILabel!
+    @IBOutlet weak var lmgLogo: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+    }
+}
