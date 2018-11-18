a37de59e82a31ca75409fcaf0237b449112fac5d
Fateme' Kazemi <fatemekazemi@Fatemes-Mac-mini.local>
2018-11-18 16:03:05 +0330

    Phone Confirmation view controller #35

---
+++ MobileToken/ui/registration/phoneconfirmation/PhoneConfirmationViewController.swift
@@ -0,0 +1,54 @@
+//
+//  PhoneConfirmationViewController.swift
+//  MobileToken
+//
+//  Created by Fateme' Kazemi on 8/27/1397 AP.
+//  Copyright © 1397 ba24.ir. All rights reserved.
+//
+
+import UIKit
+
+class PhoneConfirmationViewController: UIViewController {
+
+    @IBOutlet var vChangeNumber: UIView!
+    @IBOutlet var tfCode: UITextField!
+    @IBOutlet var vCode: UIView!
+    @IBOutlet var lbTitle: UILabel!
+    @IBOutlet var lbPhone: UILabel!
+    @IBOutlet var lbChangeNumber: UILabel!
+    @IBOutlet var lbCounter: UILabel!
+    @IBOutlet var barbtBack: UIBarButtonItem!
+    @IBOutlet var barbtConfirm: UIBarButtonItem!
+    @IBOutlet var navigationTitle: UINavigationItem!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initUIComponent()
+        // Do any additional setup after loading the view.
+    }
+    
+    func initUIComponent() {
+        lbTitle.font = UIHelper.iranSanseBold(size: 16)
+        vCode.layer.cornerRadius = 10
+        vChangeNumber.layer.cornerRadius = 10
+        lbPhone.font = UIHelper.iranSanseMedium(size: 16)
+        lbCounter.font = UIHelper.iranSanseBold(size: 16)
+        lbChangeNumber.font = UIHelper.iranSanseBold(size: 16)
+        let imageView = UIImageView()
+        let image = R.image.key()
+        imageView.image = image
+        tfCode.leftViewMode = .unlessEditing
+        barbtConfirm.setTitleTextAttributes([ NSAttributedString.Key.font:R.font.iranSansMobileBold(size: 14)!], for: .normal)
+    
+        
+    }
+
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+    
+
+}
