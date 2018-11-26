0778d82e923ce931f9b46a9abec820cd01170585
Fateme' Kazemi <fatemekazemi@Fatemes-Mac-mini.local>
2018-11-26 16:58:39 +0330

    Main view controller #48

---
+++ MobileToken/ui/main/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  MobileToken
+//
+//  Created by Fateme' Kazemi on 9/5/1397 AP.
+//  Copyright © 1397 ba24.ir. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
