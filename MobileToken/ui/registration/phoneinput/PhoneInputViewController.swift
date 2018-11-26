8d1e3fb2ac3ef4d5bd31bed8f1e2584b83260239
Fateme' Kazemi <fatemekazemi@Fatemes-Mac-mini.local>
2018-11-26 13:49:03 +0330

    Enhancements #42

--- MobileToken/ui/registration/phoneinput/PhoneInputViewController.swift
+++ MobileToken/ui/registration/phoneinput/PhoneInputViewController.swift
@@ -130,3 +130,36 @@ class PhoneInputViewController: UIViewController, BankCollectionViewDelegate,Cou
         }
     }
 }
+extension UITextField{
+    
+    @IBInspectable var doneAccessory: Bool{
+        get{
+            return self.doneAccessory
+        }
+        set (hasDone) {
+            if hasDone{
+                addDoneButtonOnKeyboard()
+            }
+        }
+    }
+    
+    func addDoneButtonOnKeyboard()
+    {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .black
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "ثبت نام", style: .done, target: self, action: #selector(self.doneButtonAction))
+        
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        
+        self.inputAccessoryView = doneToolbar
+    }
+    
+    @objc func doneButtonAction()
+    {
+        self.resignFirstResponder()
+    }
+}
