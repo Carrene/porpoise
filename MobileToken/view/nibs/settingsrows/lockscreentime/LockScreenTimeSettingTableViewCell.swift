36a99709cc848059f61c251f69e1c7b1ffb37735
Fateme' Kazemi <fatemekazemi@Fatemes-Mac-mini.local>
2018-11-29 14:25:49 +0330

    Setting viewcontroller #53

--- MobileToken/view/nibs/settingsrows/lockscreentime/LockScreenTimeSettingTableViewCell.swift
+++ MobileToken/view/nibs/settingsrows/lockscreentime/LockScreenTimeSettingTableViewCell.swift
@@ -12,18 +12,37 @@ class LockScreenTimeSettingTableViewCell: UITableViewCell {
     @IBOutlet var viewCell: UIView!
     @IBOutlet var viewButtons: UIView!
     @IBOutlet var collectionButtonsTime: [UIButton]!
+    @IBOutlet var labelSecond: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         viewButtons.layer.cornerRadius = 5
-        for index in collectionButtonsTime.indices {
-            collectionButtonsTime[index].layer.cornerRadius = 10
-            collectionButtonsTime[index].layer.borderWidth = 2
-            collectionButtonsTime[index].layer.borderColor = R.color.buttonColor()?.cgColor
-            collectionButtonsTime[index].backgroundColor = R.color.primaryDark()
+        for buttonTime in collectionButtonsTime {
+            buttonTime.layer.cornerRadius = 10
+            buttonTime.layer.borderWidth = 2
+            buttonTime.layer.borderColor = R.color.buttonColor()?.cgColor
+            buttonTime.backgroundColor = R.color.primaryDark()
         }
     }
 
+    @IBAction func onTimeButton(_ sender: UIButton) {
+        
+        for index in collectionButtonsTime.indices {
+            let button  = collectionButtonsTime[index]
+            if index == collectionButtonsTime.index(of: sender)
+            {
+                labelSecond.text =  button.currentTitle! + "" + R.string.localizable.lb_seconds()
+                button.layer.backgroundColor = R.color.buttonColor()?.cgColor
+                button.setTitleColor(R.color.primaryDark(), for: .normal)
+            }
+            else {
+                button.layer.borderColor = R.color.buttonColor()?.cgColor
+                button.backgroundColor = R.color.primaryDark()
+                button.setTitleColor(R.color.buttonColor(), for: .normal)
+            }
+        }
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
