34f343e05bd904454df8dee0fd8318bfc1ae24c5
Fateme' Kazemi <fatemekazemi@Fatemes-Mac-mini.local>
2018-12-01 11:26:39 +0330

    Setting authentication definition view and controller #56

---
+++ MobileToken/ui/settings/settingauthenticationdefinition/pattern/SettingAuthenticationDefinitionPatternViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AuthenticationDefinitionPatternViewController.swift
+//  MobileToken
+//
+//  Created by Fateme' Kazemi on 9/10/1397 AP.
+//  Copyright © 1397 ba24.ir. All rights reserved.
+//
+
+import UIKit
+
+class SettingAuthenticationDefinitionPatternViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
