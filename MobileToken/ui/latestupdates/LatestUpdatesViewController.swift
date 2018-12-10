c5a2201c96b5f14941a6a0606091dd3a2c830e7f
Fateme' Kazemi <fatemekazemi@Fatemes-Mac-mini.local>
2018-12-10 18:58:32 +0330

    Update page view #68

--- MobileToken/ui/latestupdates/LatestUpdatesViewController.swift
+++ MobileToken/ui/latestupdates/LatestUpdatesViewController.swift
@@ -10,12 +10,16 @@ import UIKit
 
 class LatestUpdatesViewController: UIViewController {
 
+    @IBOutlet var buttonEnterProgram: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        initUIComponent()
         // Do any additional setup after loading the view.
     }
     
-
+    func initUIComponent() {
+        buttonEnterProgram.layer.cornerRadius = 10
+    }
 
 }
