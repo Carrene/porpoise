d56ab7e19028d66a81d21580dcd2e295d05d0036
Fateme' Kazemi <fatemekazemi@Fatemes-Mac-mini.local>
2018-12-10 18:40:55 +0330

    Update page view #68

---
+++ MobileToken/ui/latestupdates/LatestUpdatesViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LatestUpdatesViewController.swift
+//  MobileToken
+//
+//  Created by Fateme' Kazemi on 9/19/1397 AP.
+//  Copyright © 1397 ba24.ir. All rights reserved.
+//
+
+import UIKit
+
+class LatestUpdatesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
