dffe709f7d4e29b8adf0704a5feb5d280917c11b
Fateme' Kazemi <fatemekazemi@Fatemes-Mac-mini.local>
2018-12-11 16:07:32 +0330

    Setting row problems closed #74

--- MobileToken/view/nibs/settingsrows/lockscreentime/LockScreenTimeSettingTableViewCell.swift
+++ MobileToken/view/nibs/settingsrows/lockscreentime/LockScreenTimeSettingTableViewCell.swift
@@ -1,11 +1,3 @@
-//
-//  LockScreenTimeTableViewCell.swift
-//  MobileToken
-//
-//  Created by Fateme' Kazemi on 9/7/1397 AP.
-//  Copyright © 1397 ba24.ir. All rights reserved.
-//
-
 import UIKit
 
 class LockScreenTimeSettingTableViewCell: UITableViewCell {
@@ -14,58 +6,64 @@ class LockScreenTimeSettingTableViewCell: UITableViewCell {
     @IBOutlet var viewButtons: UIView!
     @IBOutlet var collectionButtonsTime: [UIButton]!
     @IBOutlet var labelSecond: UILabel!
+    
     var selectedIndex = 1
     
     override func awakeFromNib() {
         
         super.awakeFromNib()
+        
         viewButtons.layer.cornerRadius = 5
         for index in collectionButtonsTime.indices {
             
             let button  = collectionButtonsTime[index]
             button.layer.cornerRadius = 10
             button.layer.borderWidth = 2
+            button.setTitleColor(R.color.primaryDark(), for: .selected)
+            button.setTitleColor(R.color.buttonColor(), for: .normal)
+            button.tintColor = .clear
+            
             if index == selectedIndex {
-                
-                labelSecond.text =  button.currentTitle! + " " + R.string.localizable.lb_seconds()
-                button.layer.backgroundColor = R.color.buttonColor()?.cgColor
-                button.setTitleColor(R.color.primaryDark(), for: .normal)
+                setSelected(button: button)
             }
             else {
-                
-                button.layer.borderColor = R.color.buttonColor()?.cgColor
-                button.backgroundColor = R.color.primaryDark()
-                button.setTitleColor(R.color.buttonColor(), for: .normal)
+                setDeselect(button: button)
             }
         }
     }
-
+    
+    private func setSelected(button:UIButton){
+        labelSecond.text =  button.currentTitle! + " " + R.string.localizable.lb_seconds()
+        button.layer.backgroundColor = R.color.buttonColor()?.cgColor
+        button.isSelected = true
+        
+    }
+    
+    private func setDeselect(button:UIButton){
+        button.layer.borderColor = R.color.buttonColor()?.cgColor
+        button.backgroundColor = R.color.primaryDark()
+        button.isSelected = false
+        
+    }
+    
     @IBAction func onTimeButton(_ sender: UIButton) {
         
-        for index in collectionButtonsTime.indices {
-            
-            let button  = collectionButtonsTime[index]
-            if index == collectionButtonsTime.index(of: sender)
-            {
-                selectedIndex = index
-                labelSecond.text =  button.currentTitle! + " " + R.string.localizable.lb_seconds()
-                button.layer.backgroundColor = R.color.buttonColor()?.cgColor
-                button.setTitleColor(R.color.primaryDark(), for: .normal)
-            }
-            else {
-                
-                button.layer.borderColor = R.color.buttonColor()?.cgColor
-                button.backgroundColor = R.color.primaryDark()
-                button.setTitleColor(R.color.buttonColor(), for: .normal)
-            }
-        }
+                for index in collectionButtonsTime.indices {
+                    let button  = collectionButtonsTime[index]
+                    if index == collectionButtonsTime.index(of: sender)
+                    {
+                        selectedIndex = index
+                        setSelected(button: button)
+                    }
+                    else {
+        
+                        setDeselect(button: button)
+                    }
+                }
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
-        
         super.setSelected(selected, animated: animated)
         self.selectionStyle = .none
-        // Configure the view for the selected state
     }
-    
 }
