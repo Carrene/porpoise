dcbe8d26b3c045f581dbc174c4d533643798be4a
Fateme' Kazemi <fatemekazemi@Fatemes-Mac-mini.local>
2018-12-24 16:57:22 +0330

    Mediator setting closed #92

--- MobileToken/ui/settings/basic/SettingMediator.swift
+++ MobileToken/ui/settings/basic/SettingMediator.swift
@@ -1,1 +1,24 @@
 import Foundation
+
+class SettingMediator {
+    
+    private var setting : Setting?
+    private var authentication : Authentication?
+    
+    init(setting:Setting ,  authentication:Authentication) {
+        self.setting = setting
+        self.authentication = authentication
+    }
+    
+    func setSetting(setting:Setting) {
+        self.setting = setting
+    }
+    
+    func getSetting() -> Setting {
+        return self.setting!
+    }
+    
+    func getAuthentication() -> Authentication {
+        return self.authentication!
+    }
+}
