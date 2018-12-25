027cd641c3e85b81480c692e0dfcd1ebc446ad9f
Fateme' Kazemi <fatemekazemi@Fatemes-Mac-mini.local>
2018-12-25 13:54:05 +0330

    Settings presenter #55

--- MobileToken/ui/settings/basic/SettingPresenter.swift
+++ MobileToken/ui/settings/basic/SettingPresenter.swift
@@ -4,13 +4,15 @@ class SettingPresenter:SettingPresenterProtocol {
     
     var settingMediator : SettingMediator?
     unowned let settingView : SettingViewProtocol
+    var setting:Setting?
+    var authentication:Authentication?
     
     required init(settingView:SettingViewProtocol) {
         self.settingView = settingView
     }
     
     func getAllDataSetting() {
-        settingMediator = SettingMediator(setting: <#T##Setting#>, authentication: <#T##Authentication#>)
+        settingMediator = SettingMediator(setting: getSetting(), authentication: getAuthentication())
         
     }
     
@@ -18,38 +20,38 @@ class SettingPresenter:SettingPresenterProtocol {
         
     }
     
-    func getAuthentication() {
+    func getAuthentication() -> Authentication{
         let authenticationRepository = AuthenticationRealmRepository()
         let onDataResponse: ((RepositoryResponse<Authentication>) -> ()) = {[weak self] repoResponse in
             if repoResponse.error != nil {
                 UIHelper.showFailedSnackBar()
             } else {
                 if let authentication = repoResponse.value {
-                    if authentication.authenticationType == AuthenticationTypeEnum.PASSWORD {
-                        
-                    } else {
-                        
-                    }
+                    self?.authentication = authentication
+                } else {
+                    UIHelper.showFailedSnackBar()
                 }
             }
         }
         authenticationRepository.get(onDone: onDataResponse)
+        return authentication!
     }
     
-    func getSetting() {
+    func getSetting() -> Setting{
         let settingRepository = SettingRealmRepository()
         let onDataResponse: ((RepositoryResponse<Setting>) -> ()) = {[weak self] repoResponse in
             if repoResponse.error != nil {
                 UIHelper.showFailedSnackBar()
             } else {
                 if let setting = repoResponse.value {
-                    
+                    self?.setting = setting
                 } else {
-                    
+                    UIHelper.showFailedSnackBar()
                 }
             }
         }
         settingRepository.get(onDone: onDataResponse)
+        return setting!
     }
     
     
