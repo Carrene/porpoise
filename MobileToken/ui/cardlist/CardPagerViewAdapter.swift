6b4237b458cf51fd973c6b1e37cddcdcce1358cd
Fateme' Kazemi <fatemekazemi@Fatemes-Mac-mini.local>
2019-01-10 10:48:41 +0330

    Cardlist viewcontroller #87

--- MobileToken/ui/cardlist/CardPagerViewAdapter.swift
+++ MobileToken/ui/cardlist/CardPagerViewAdapter.swift
@@ -21,7 +21,7 @@ class CardPagerViewAdapter:NSObject, FSPagerViewDelegate, FSPagerViewDataSource 
     func pagerView(_ pagerView: FSPagerView, cellForItemAt index: Int) -> FSPagerViewCell {
         if index == 0 {
             let cell = pagerView.dequeueReusableCell(withReuseIdentifier: R.nib.bankCardPagerViewCell.identifier, at: index) as! BankCardPagerViewCell
-            cell.vCard.backgroundColor = R.color.primary()
+            //cell.vCard.backgroundColor = R.color.primary()
             return cell
         }else {
             
--- MobileToken/view/nibs/bankcard/BankCardPagerViewCell.swift
+++ MobileToken/view/nibs/bankcard/BankCardPagerViewCell.swift
@@ -6,11 +6,16 @@ class BankCardPagerViewCell: FSPagerViewCell {
 
     
     @IBOutlet weak var vCard: CardCellXibView!
+    @IBOutlet var viewBankCard: UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-        
+        vCard.layer.cornerRadius = 10
+        vCard.layer.borderColor = R.color.buttonColor()?.withAlphaComponent(0.5).cgColor
+        vCard.layer.borderWidth = 0.5
+        viewBankCard.layer.cornerRadius = 10
+        viewBankCard.layer.borderColor = R.color.buttonColor()?.withAlphaComponent(0.5).cgColor
+        viewBankCard.layer.borderWidth = 0.5
     }
     
     
