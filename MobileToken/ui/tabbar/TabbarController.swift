554d483a61478d3a64a598551514d905488b5472
Fateme' Kazemi <fatemekazemi@Fatemes-Mac-mini.local>
2019-01-10 10:59:31 +0330

    Cardlist viewcontroller #87

--- MobileToken/ui/tabbar/TabbarController.swift
+++ MobileToken/ui/tabbar/TabbarController.swift
@@ -13,21 +13,6 @@ class TabBarViewController: UITabBarController, UITabBarControllerDelegate {
     }
 
     func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
-        let index = tabBarController.selectedIndex
-//        if index == NSNotFound || index == 1 {
-//            let navController = tabBarController.viewControllers?[tabBarController.selectedIndex] as? UINavigationController
-//            //            navController?.popToRootViewController(animated: false)
-//            return
-//        }
-//
-//        if index == NSNotFound || index == 3 {
-//            let navController = tabBarController.viewControllers?[tabBarController.selectedIndex] as? UINavigationController
-//            for vc in (navController?.viewControllers)! {
-//                if vc is TransactionHistoryViewController {
-//                    (vc as! TransactionHistoryViewController).stateHelper = StateHelper()
-//                }
-//            }
-//            navController?.popToRootViewController(animated: false)
-//        }
+
     }
 }
