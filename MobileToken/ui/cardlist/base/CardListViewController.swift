e6fea8ddad0fdff51b714e053038e6954d9ed5be
Fateme' Kazemi <fatemekazemi@Fatemes-Mac-mini.local>
2019-01-26 14:47:25 +0330

    Custom alert dialog #101

--- MobileToken/ui/cardlist/base/CardListViewController.swift
+++ MobileToken/ui/cardlist/base/CardListViewController.swift
@@ -65,20 +65,25 @@ class CardListViewController: BaseViewController,CardListViewProtocol,CardPagerV
         let rect = CGRect(x: margin, y: margin, width: 300, height: 310)
         let customView = UIView(frame: rect)
         
-        let buttonDeleteFirstToken = UIButton(frame: CGRect(x: 40, y: 60, width: 295, height: 40))
+        let buttonDeleteFirstToken = UIButton(frame: CGRect(x: 45, y: 60, width: deleteTokenAlert.view.frame.width*0.6, height: 40))
         buttonDeleteFirstToken.layer.cornerRadius = 10
         buttonDeleteFirstToken.layer.borderColor = R.color.buttonColor()?.cgColor
         buttonDeleteFirstToken.backgroundColor = .clear
         buttonDeleteFirstToken.layer.borderWidth = 1
-        buttonDeleteFirstToken.setTitle("حذف رمز اول کارت", for: .normal)
+        buttonDeleteFirstToken.setTitle(R.string.localizable.alert_delete_first_token(), for: .normal)
+        buttonDeleteFirstToken.setTitleColor(R.color.buttonColor(), for: .normal)
+        buttonDeleteFirstToken.setTitleColor(R.color.secondary(), for: .selected)
         buttonDeleteFirstToken.titleLabel?.font = R.font.iranSansMobileBold(size: 16)
         
-        let buttonDeleteSecondToken = UIButton(frame: CGRect(x: 40, y: 114, width: 295, height: 40))
+        let buttonDeleteSecondToken = UIButton(frame: CGRect(x: 45, y: 114, width: deleteTokenAlert.view.frame.width*0.6, height: 40))
         buttonDeleteSecondToken.layer.cornerRadius = 10
         buttonDeleteSecondToken.layer.borderColor = R.color.buttonColor()?.cgColor
+        buttonDeleteSecondToken.setTitleColor(R.color.buttonColor(), for: .normal)
+        buttonDeleteSecondToken.setTitleColor(R.color.secondary(), for: .selected)
         buttonDeleteSecondToken.backgroundColor = .clear
         buttonDeleteSecondToken.layer.borderWidth = 1
-        buttonDeleteSecondToken.setTitle("حذف رمز دوم کارت,", for: .normal)
+        buttonDeleteSecondToken.setTitle(
+            R.string.localizable.alert_delete_second_token(), for: .normal)
         buttonDeleteSecondToken.titleLabel?.font = R.font.iranSansMobileBold(size: 16)
         
         customView.addSubview(buttonDeleteFirstToken)
@@ -101,19 +106,21 @@ class CardListViewController: BaseViewController,CardListViewProtocol,CardPagerV
         let deleteFirstTokenAction = UIAlertAction(title: R.string.localizable.alert_delete_first_token() , style: .default , handler: { alert -> Void in
             
         })
-        let saveAction = UIAlertAction(title: R.string.localizable.save() , style: .default, handler: { (action : UIAlertAction!) -> Void in })
-        
-        let cancelAction = UIAlertAction(title: R.string.localizable.cancel() , style: .destructive, handler: {
+        let saveAction = UIAlertAction(title: R.string.localizable.ash_delete_token() , style: .default, handler: { (action : UIAlertAction!) -> Void in })
+      
+        let cancelAction = UIAlertAction(title: R.string.localizable.cancel() , style: .default, handler: {
             (action : UIAlertAction!) -> Void in })
         
-        let height:NSLayoutConstraint = NSLayoutConstraint(item: deleteTokenAlert.view, attribute: NSLayoutConstraint.Attribute.height, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1, constant: self.view.frame.height * 0.30)
+        let height:NSLayoutConstraint = NSLayoutConstraint(item: deleteTokenAlert.view, attribute: NSLayoutConstraint.Attribute.height, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1, constant: self.view.frame.height * 0.33)
+        
         deleteTokenAlert.view.addConstraint(height)
         
-        let width:NSLayoutConstraint = NSLayoutConstraint(item: deleteTokenAlert.view, attribute: NSLayoutConstraint.Attribute.width, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1, constant: self.view.frame.width * 0.90)
+        let width:NSLayoutConstraint = NSLayoutConstraint(item: deleteTokenAlert.view, attribute: NSLayoutConstraint.Attribute.width, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1, constant: self.view.frame.width * 0.9)
         deleteTokenAlert.view.addConstraint(width)
         
-        deleteTokenAlert.addAction(saveAction)
         deleteTokenAlert.addAction(cancelAction)
+        deleteTokenAlert.addAction(saveAction)
+        
         
         DispatchQueue.main.async {
             self.present(deleteTokenAlert, animated: true, completion:{})
