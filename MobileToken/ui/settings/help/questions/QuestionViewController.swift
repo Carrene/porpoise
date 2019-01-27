5b4d71e8b37defd26c7d5a7dfb1b77bc83bcd4d1
Fateme' Kazemi <fatemekazemi@Fatemes-Mac-mini.local>
2019-01-27 14:54:00 +0330

    FAQ #122

---
+++ MobileToken/ui/settings/help/questions/QuestionViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+class QuestionViewController: BaseViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func initUIComponents() {
+        
+    }
+    
+    func initListeners() {
+        
+    }
+    
+
+}
