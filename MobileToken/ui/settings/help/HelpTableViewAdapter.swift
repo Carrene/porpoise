d1968f83ed8c165291053f8c0655c7e0ac071642
Fateme' Kazemi <fatemekazemi@Fatemes-Mac-mini.local>
2019-01-27 14:41:35 +0330

    FAQ #122

--- MobileToken/ui/settings/help/HelpTableViewAdapter.swift
+++ MobileToken/ui/settings/help/HelpTableViewAdapter.swift
@@ -3,7 +3,8 @@ import UIKit
 
 class HelpTableViewAdapter : NSObject,UITableViewDelegate,UITableViewDataSource {
     
-    private var dataSource: [String]?
+    private var dataSource = [R.string.localizable.help_add_bank(),R.string.localizable.help_add_card(),R.string.localizable.help_edit_cardName(),R.string.localizable.help_delete_card(),R.string.localizable.help_get_token(),R.string.localizable.help_add_token(),R.string.localizable.help_change_token(),R.string.localizable.help_custom_token(),R.string.localizable.help_copy_token(),R.string.localizable.help_delete_token(),R.string.localizable.help_edit_phone()]
+    
     private var sender:HelpViewController?
     
     func setDataSource(dataSource:[String]) {
@@ -15,17 +16,17 @@ class HelpTableViewAdapter : NSObject,UITableViewDelegate,UITableViewDataSource 
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.dataSource?.count ?? 100
+        return self.dataSource.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: R.reuseIdentifier.reuseHelpCell.identifier, for: indexPath) as! HelpTableViewCell
-        cell.textLabel?.text = "چطور میتوانم بانک جدید اضافه کنم؟"
-        cell.textLabel?.numberOfLines = 0
-        cell.textLabel?.textAlignment = .right
-        cell.textLabel?.font = R.font.iranSansMobile(size: 16)
-        cell.textLabel?.textColor = R.color.buttonColor()
+        cell.textLabel?.text = dataSource[indexPath.row]
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
     }
     
     
--- MobileToken/ui/settings/help/HelpTableViewCell.swift
+++ MobileToken/ui/settings/help/HelpTableViewCell.swift
@@ -4,12 +4,15 @@ class HelpTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        self.textLabel?.numberOfLines = 0
+        self.textLabel?.textAlignment = .right
+        self.textLabel?.font = R.font.iranSansMobile(size: 16)
+        self.textLabel?.textColor = R.color.buttonColor()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         
     }
 
