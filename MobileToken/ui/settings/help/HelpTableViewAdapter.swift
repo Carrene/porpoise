92056e540d4971711306955e5dac378ec16edd0a
Fateme' Kazemi <fatemekazemi@Fatemes-Mac-mini.local>
2019-01-27 12:11:18 +0330

    FAQ #122

--- MobileToken/ui/settings/help/HelpTableViewAdapter.swift
+++ MobileToken/ui/settings/help/HelpTableViewAdapter.swift
@@ -1,1 +1,46 @@
 import Foundation
+import UIKit
+
+class HelpTableViewAdapter : NSObject,UITableViewDelegate,UITableViewDataSource {
+    
+    private var dataSource: [String]?
+    private var sender:HelpViewController?
+    
+    func setDataSource(dataSource:[String]) {
+        self.dataSource = dataSource
+    }
+    
+    init(sender:HelpViewController) {
+        self.sender = sender
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.dataSource?.count ?? 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: R.reuseIdentifier.reuseHelpCell.identifier, for: indexPath) as! HelpTableViewCell
+        cell.textLabel?.text = "چطور میتوانم بانک جدید اضافه کنم؟"
+        cell.textLabel?.numberOfLines = 0
+        cell.textLabel?.textAlignment = .right
+        cell.textLabel?.font = R.font.iranSansMobile(size: 16)
+        cell.textLabel?.textColor = R.color.buttonColor()
+        return cell
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
--- MobileToken/ui/settings/help/HelpViewController.swift
+++ MobileToken/ui/settings/help/HelpViewController.swift
@@ -2,19 +2,34 @@ import UIKit
 
 class HelpViewController: BaseViewController {
    
-
+    @IBOutlet var tableView: UITableView!
+    private var adapter : HelpTableViewAdapter?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.estimatedRowHeight = 80
+        tableView.rowHeight = UITableView.automaticDimension
+        
     }
     
     func initUIComponents() {
-        
+        initTableView()
     }
     
     func initListeners() {
         
+    }
+    
+    func initTableView() {
+//        tableView?.register(UINib(nibName:R.nib.lockScreenTimeTableViewCell.name,bundle: nil),forCellReuseIdentifier:R.reuseIdentifier.reuseLockScreenTimerSettingRow.identifier)
+//        tableView?.register(UINib(nibName:R.nib.authenticationTypeTableViewCell.name,bundle: nil),forCellReuseIdentifier:R.reuseIdentifier.reuseAuthenticationTypeSettingRow.identifier)
+        self.adapter = HelpTableViewAdapter(sender: self)
+        //adapter!.setDelegate(settingTableAdapterProtocol: self)
+        tableView.delegate = adapter
+        tableView.dataSource = adapter
+        tableView.tableFooterView = UIView()
+        tableView.backgroundColor = R.color.primary()
+        tableView.reloadData()
     }
     
 }
