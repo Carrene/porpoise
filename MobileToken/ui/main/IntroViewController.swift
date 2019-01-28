47de0ef43d48518136c318282f8c2420184315e6
Fateme' Kazemi <fatemekazemi@Fatemes-Mac-mini.local>
2019-01-28 13:30:43 +0330

    New design changes #96

--- MobileToken/ui/main/IntroViewController.swift
+++ MobileToken/ui/main/IntroViewController.swift
@@ -41,10 +41,10 @@ class IntroViewController {
             pages[i].viewWillAppearBlock = {
                 pages[i].view.backgroundColor = UIColor(netHex: Int(pageColors[i], radix: 16)!)
             }
-            pages[i].topPadding = 59
-            pages[i].underIconPadding = 147
+            pages[i].topPadding = 50
+            pages[i].underIconPadding = 50
             pages[i].underTitlePadding = 20
-            pages[i].bottomPadding = 30
+            pages[i].bottomPadding = 20
             let gif : UIImage?
             switch i {
             case 0:
@@ -52,8 +52,9 @@ class IntroViewController {
                 pages[0].iconImageView = UIImageView(gifImage: gif!, loopCount: -1)
                 pages[0].iconHeight = 110
                 pages[0].iconWidth = 140
-                pages[0].topPadding = 147
-                pages[0].underIconPadding = 217
+                pages[0].topPadding = 150
+                pages[0].underIconPadding = 70
+                
             case 2:
                 gif = UIImage(gifName: R.image.forgetPasswordGif.name)
                 pages[2].iconImageView = UIImageView(gifImage: gif!, loopCount: -1)
