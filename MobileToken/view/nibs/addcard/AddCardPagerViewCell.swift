ee3e35aabd059879a035cfd9376b6ca8bdddf86c
Fateme' Kazemi <fatemekazemi@Fatemes-Mac-mini.local>
2019-02-23 14:43:03 +0330

    New design #84

--- MobileToken/view/nibs/addcard/AddCardPagerViewCell.swift
+++ MobileToken/view/nibs/addcard/AddCardPagerViewCell.swift
@@ -19,20 +19,30 @@ class AddCardPagerViewCell: FSPagerViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         viewCard.layer.cornerRadius = 10
+        buttonAddCard.layer.cornerRadius = 10
+        
         viewAddCard.layer.cornerRadius = 10
         viewAddCard.layer.shadowPath = UIBezierPath(roundedRect: viewAddCard.bounds, cornerRadius: 10).cgPath
         viewAddCard.layer.shadowRadius = 3
         viewAddCard.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
         viewAddCard.layer.shadowOpacity = 0.2
         viewAddCard.layer.backgroundColor = R.color.primaryLight()?.cgColor
-        viewAddCard.layer.shadowColor = R.color.buttonColor()?.withAlphaComponent(0.25).cgColor
-        viewCard.layer.borderColor = R.color.buttonColor()?.withAlphaComponent(0.5).cgColor
-        viewCard.layer.borderWidth = 0.5
+        viewAddCard.layer.shadowColor = R.color.buttonColor()?.withAlphaComponent(0.15).cgColor
+        
+        viewCard.layer.cornerRadius = 10
+        viewCard.layer.shadowPath = UIBezierPath(roundedRect: viewAddCard.bounds, cornerRadius: 10).cgPath
+        viewCard.layer.shadowRadius = 3
+        viewCard.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+        viewCard.layer.shadowOpacity = 0.2
+        viewCard.layer.backgroundColor = R.color.primaryLight()?.cgColor
+        viewCard.layer.shadowColor = R.color.buttonColor()?.withAlphaComponent(0.15).cgColor
+        
+        
         viewCard.labelCardName.text = R.string.localizable.lb_cardName()
         viewCard.buttonActionSheet.isEnabled = false
         viewCard.labelBottomTitle.text = R.string.localizable.lb_add_new_bank()
         viewCard.stackViewCardNumber.isHidden = true
-        viewAddCard.layer.cornerRadius = 10
+        
         textFieldCardName.layer.cornerRadius = 10
         textFieldCardName.layer.borderColor = R.color.buttonColor()?.withAlphaComponent(0.5).cgColor
         textFieldCardName.layer.borderWidth = 2
