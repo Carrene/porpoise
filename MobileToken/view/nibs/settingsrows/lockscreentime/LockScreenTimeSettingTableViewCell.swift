d862f7bf2f4023cbde559d61590f247c895881de
Fateme' Kazemi <fatemekazemi@Fatemes-Mac-mini.local>
2019-02-24 16:23:03 +0330

    New design #84

--- MobileToken/view/nibs/settingsrows/lockscreentime/LockScreenTimeSettingTableViewCell.swift
+++ MobileToken/view/nibs/settingsrows/lockscreentime/LockScreenTimeSettingTableViewCell.swift
@@ -18,14 +18,26 @@ class LockScreenTimeSettingTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         
         super.awakeFromNib()
-        
+        viewButtons.layer.shadowPath = UIBezierPath(roundedRect: viewButtons.bounds, cornerRadius: 5).cgPath
+        viewButtons.layer.shadowRadius = 3
+        viewButtons.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+        viewButtons.layer.shadowOpacity = 0.2
+        viewButtons.layer.backgroundColor = R.color.primaryLight()?.cgColor
+        viewButtons.layer.shadowColor = R.color.buttonColor()?.withAlphaComponent(0.25).cgColor
         viewButtons.layer.cornerRadius = 5
+        
         for index in collectionButtonsTime.indices {
             
             let button  = collectionButtonsTime[index]
             button.layer.cornerRadius = 10
+            button.layer.shadowPath = UIBezierPath(roundedRect: button.bounds, cornerRadius: 10).cgPath
+            button.layer.shadowRadius = 3
+            button.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+            button.layer.shadowOpacity = 0.2
+            button.layer.backgroundColor = R.color.primaryLight()?.cgColor
+            button.layer.shadowColor = R.color.buttonColor()?.withAlphaComponent(0.5).cgColor
             button.layer.borderWidth = 2
-            button.setTitleColor(R.color.primaryDark(), for: .selected)
+            button.setTitleColor(R.color.buttonColor(), for: .selected)
             button.setTitleColor(R.color.secondary(), for: .normal)
             button.tintColor = .clear
             
@@ -49,14 +61,13 @@ class LockScreenTimeSettingTableViewCell: UITableViewCell {
     
     private func setSelected(button:UIButton){
         labelSecond.text =  button.currentTitle! + " " + R.string.localizable.lb_seconds()
-        button.layer.backgroundColor = R.color.secondary()?.cgColor
         button.isSelected = true
+        button.layer.borderColor = R.color.secondary()?.cgColor
         
     }
     
     private func setDeselect(button:UIButton){
-        button.layer.borderColor = R.color.secondary()?.cgColor
-        button.backgroundColor = R.color.primaryDark()
+        button.layer.borderColor = R.color.primaryLight()?.cgColor
         button.isSelected = false
         
     }
