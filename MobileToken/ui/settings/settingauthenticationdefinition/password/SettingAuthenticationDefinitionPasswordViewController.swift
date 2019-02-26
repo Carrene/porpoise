0a9dd19f21c9e126372baf61e95dac5823c6fa75
Fateme' Kazemi <fatemekazemi@Fatemes-Mac-mini.local>
2019-02-26 16:05:59 +0330

    New design #84

--- MobileToken/ui/settings/settingauthenticationdefinition/password/SettingAuthenticationDefinitionPasswordViewController.swift
+++ MobileToken/ui/settings/settingauthenticationdefinition/password/SettingAuthenticationDefinitionPasswordViewController.swift
@@ -35,6 +35,7 @@ class SettingAuthenticationDefinitionPasswordViewController: UIViewController,UI
         labelPasswordHint.font = R.font.iranSansMobile(size: 12)
         labelSecondPassword.font = R.font.iranSansMobile(size: 12)
         labelPasswordHint.textColor = R.color.buttonColor()?.withAlphaComponent(0.5)
+        labelSecondPassword.textColor = R.color.buttonColor()?.withAlphaComponent(0.5)
         textFieldPassword.attributedPlaceholder = NSAttributedString(string: R.string.localizable.ph_password(),
                                                                      attributes: [NSAttributedString.Key.foregroundColor: R.color.buttonColor()!.withAlphaComponent(0.5)])
         textFieldConfirmPassword.attributedPlaceholder = NSAttributedString(string:R.string.localizable.ph_confirm_password() ,
