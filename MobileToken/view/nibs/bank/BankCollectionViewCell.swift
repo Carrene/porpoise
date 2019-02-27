724032ae6056b507c4074611b8bc5aa1f9ae7023
Fateme' Kazemi <fatemekazemi@Fatemes-Mac-mini.local>
2019-02-27 18:06:26 +0330

    New design #84

--- MobileToken/view/nibs/bank/BankCollectionViewCell.swift
+++ MobileToken/view/nibs/bank/BankCollectionViewCell.swift
@@ -14,22 +14,16 @@ class BankCollectionViewCell: UICollectionViewCell {
         view.layer.cornerRadius = 10
         //view.layer.borderWidth = 1
         //view.layer.borderColor = R.color.primaryLight()?.cgColor
-        view.layer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: 10).cgPath
-        view.layer.shadowRadius = 3
+        //view.layer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: 10).cgPath
+        view.layer.shadowRadius = 5
         view.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
-        view.layer.shadowOpacity = 0.2
+        view.layer.shadowOpacity = 0.8
         view.layer.backgroundColor = R.color.primaryLight()?.cgColor
         view.layer.shadowColor = R.color.buttonColor()?.withAlphaComponent(0.25).cgColor
         vCell.layer.borderColor = R.color.primaryLight()?.cgColor
         vCell.layer.cornerRadius = 10
         
         vCell.layer.borderWidth = 2
-//        vCell.layer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: 10).cgPath
-//        vCell.layer.shadowRadius = 5
-//        vCell.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
-//        vCell.layer.shadowOpacity = 0.2
-//        vCell.layer.backgroundColor = R.color.primaryLight()?.cgColor
-//        vCell.layer.shadowColor = R.color.buttonColor()?.withAlphaComponent(0.25).cgColor
-//        lbBankName.font = R.font.iranSansMobileBold(size: 16)
+
     }
 }
