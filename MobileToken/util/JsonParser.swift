055d39f59f23f6dc92a70d16491d16835a3cc38b
Fateme' Kazemi <fatemekazemi@Fatemes-Mac-mini.local>
2019-03-03 15:35:27 +0330

    Bank json #222

---
+++ MobileToken/util/JsonParser.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+class JsonParser:NSObject {
+    private func readJson() {
+        do {
+            if let file = Bundle.main.url(forResource: "banks", withExtension: "json") {
+                let data = try Data(contentsOf: file)
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                if let object = json as? [String: Any] {
+                    // json is a dictionary
+                    print(object)
+                } else if let object = json as? [Any] {
+                    // json is an array
+                    print(object)
+                } else {
+                    print("JSON is invalid")
+                }
+            } else {
+                print("no file")
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
